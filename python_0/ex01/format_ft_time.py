3b4b0ddfa7dd83fef15374fdc28be5abd8620e76
Franck Li <fli@f2r4s17.clusters.42paris.fr>
2025-07-08 18:04:34 +0200

    python0_ex01

--- python_0/ex01/format_ft_time.py
+++ python_0/ex01/format_ft_time.py
@@ -1,4 +1,23 @@
+import time
+from datetime import datetime
 
+def format_ft_time():
+    current_timestamp_ms = time.time()
 
+    formatted_timestamp_standard = f"{current_timestamp_ms:,.4f}"
 
-print("Seconds since January 1, 1970:")
+    formatted_timestamp_scientific = f"{current_timestamp_ms:.2e}"
+
+    dt_object = datetime.fromtimestamp(current_timestamp_ms)
+
+    formatted_date_string = dt_object.strftime("%b %d %Y")
+
+    output = (
+        f"Seconds since January 1, 1970: {formatted_timestamp_standard} "
+        f"or {formatted_timestamp_scientific} in scientific notation\n"
+        f"{formatted_date_string}"
+    )
+    print(output)
+
+if __name__ == "__main__":
+    format_ft_time()
