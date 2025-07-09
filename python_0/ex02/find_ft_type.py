9d96b1b91c721a0c8c1c43ef171828f8e4839d0b
Franck Li <fli@f2r2s17.clusters.42paris.fr>
2025-07-09 14:06:15 +0200

    python0_ex02

---
+++ python_0/ex02/find_ft_type.py
@@ -0,0 +1,31 @@
+def all_thing_is_obj(object: any) -> int:
+    if type(object) is list:
+        print(f"List : {type(object)}")
+    elif type(object) is tuple:
+        print(f"Tuple : {type(object)}")
+    elif type(object) is set:
+        print(f"Set : {type(object)}")
+    elif type(object) is dict:
+        print(f"Dict : {type(object)}")
+    elif type(object) is str:
+        print(f"{object} is in the kitchen : {type(object)}")
+    else:
+        print("Type not found")
+    return 42
+
+
+# tester.py
+# from find_ft_type import all_thing_is_obj
+
+# ft_list = ["Hello", "tata!"]
+# ft_tuple = ("Hello", "toto!")
+# ft_set = {"Hello", "tutu!"}
+# ft_dict = {"Hello" : "titi!"}
+
+# all_thing_is_obj(ft_list)
+# all_thing_is_obj(ft_tuple)
+# all_thing_is_obj(ft_set)
+# all_thing_is_obj(ft_dict)
+# all_thing_is_obj("Brian")
+# all_thing_is_obj("Toto")
+# print(all_thing_is_obj(10))
